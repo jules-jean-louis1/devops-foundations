317052c94d4a92e2d7c134eec762a02b5720403c
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2026-02-03 17:21:38 +0100

    feat: added nodemon for hotreload in dev

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -10,6 +10,28 @@ fastify.get("/", function (request, reply) {
   reply.send({ hello: "world" });
 });
 
+//healthcheck
+fastify.get("/health", function (request: any, reply: any) {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    reply.send(healthcheck);
+  } catch (e: any) {
+    healthcheck.message = e;
+    reply.status(503).send();
+  }
+});
+
+//Test DB
+fastify.get("/db", function (request: any, reply: any) {});
+
+fastify.get("/cache", function (request: any, reply: any) {});
+
+fastify.get("/contact", function (request: any, reply: any) {});
+
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {

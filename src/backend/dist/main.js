8bc99bd362c11096a08f3f8fa8dd968fb032f725
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2026-02-03 16:42:13 +0100

    feat: initialize fastify

---
+++ src/backend/dist/main.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// ESM
+const fastify_1 = __importDefault(require("fastify"));
+const fastify = (0, fastify_1.default)({
+    logger: true,
+});
+// Declare a route
+fastify.get("/", function (request, reply) {
+    reply.send({ hello: "world" });
+});
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    // Server is now listening on ${address}
+});
+//# sourceMappingURL=main.js.map
